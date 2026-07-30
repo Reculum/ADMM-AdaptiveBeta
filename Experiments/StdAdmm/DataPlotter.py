9b9f3cc2228ec3d98d796b06f066a6e42d92e3ff
Reculum <federicomenegazzo01@gmail.com>
2026-07-30 23:37:00 +0200

    v1.1

--- Experiments/StdAdmm/DataPlotter.py
+++ Experiments/StdAdmm/DataPlotter.py
@@ -13,49 +13,121 @@ def Plot(npzPath):
 
     PrimalRes = data["PrimalRes"]
     DualRes = data["DualRes"]
+    IMGs = data["IMGs"]
 
     CpuTimes = data["CpuTimes"]
-    ConvergenceDistance = data["ConvergenceDistance"]
 
     xTrue = data["xTrue"]
-    xCorrupted = data["xCorrupted"]  
-
-    
+    xCorrupted = data["xCorrupted"]
+    x0 = data["x0"]
+    y0 = data["y0"]     
 
     iters, n = np.shape(Xs)
+    cut = int(iters * 0.9)
+    
+    xReconstr = Xs[-1, :]
+    ImgXreconstr = IMGs[-1]
 
-    xSol = Xs[iters - 1, :]
-    x0 = Xs[0, :]
+    DomainDistance = np.linalg.norm(Xs - xReconstr, axis=1)
+    ImgDistance = np.abs(IMGs - ImgXreconstr)
+
+    DomainRatios = DomainDistance[1:] / DomainDistance[:-1]
+    ImageRatios = ImgDistance[1:] / ImgDistance[:-1]
+
+    plt.figure(0)
+
+    plt.subplot(1,2,1)
+    plt.title("x0")
+    plt.plot(x0)
+    plt.xlabel("components")
+    plt.grid() 
+
+    plt.subplot(1,2,2)
+    plt.title("y0")
+    plt.plot(y0)
+    plt.xlabel("components")
+    plt.grid()
+
+    plt.savefig("startingPoints.pdf", format="pdf", bbox_inches="tight")
+
+
+    ###########################################
 
     plt.figure(1)
     plt.grid()
     plt.title("Corrupted vs Reconstruction vs True")
     plt.plot(xTrue)
     plt.plot(xCorrupted)
-    plt.plot(xSol)
+    plt.plot(xReconstr)
     plt.legend(["True", "Corrupted", "Reconstruction"])
-    plt.xlabel("compontents")
-    
+    plt.xlabel("components")
+    plt.savefig("reconstr.pdf", format="pdf", bbox_inches="tight")
+
+    ###########################################
 
     plt.figure(2)
     plt.grid()
-    plt.title("Metrics vs Iters")
-    plt.loglog(ConvergenceDistance)
-    plt.loglog(PrimalRes)
-    plt.loglog(DualRes)
+    plt.title("Residuals vs Iters")
+    plt.semilogy(PrimalRes)
+    plt.semilogy(DualRes)
     plt.xlabel("iters")
-    plt.legend(["xSolDistance", "PrimalRes", "DualRes"])
+    plt.legend(["PrimalRes", "DualRes"])
+    plt.savefig("residuals.pdf", format="pdf", bbox_inches="tight")
+
+    ###########################################
 
     plt.figure(3)
     plt.grid()
-    plt.title("Metrics vs Cpu Time")
-    plt.semilogy(CpuTimes, ConvergenceDistance)
-    plt.semilogy(CpuTimes, PrimalRes)
-    plt.semilogy(CpuTimes, DualRes)
-    plt.xlabel("Runtime (s)")
-    plt.legend(["xSolDistance", "PrimalRes", "DualRes"])
+    plt.title("Pen. Param. vs Iters")
+    plt.plot(Betas)
+    plt.xlabel("iters")
+    plt.legend(["Beta"])
+    plt.savefig("betas.pdf", format="pdf", bbox_inches="tight")
 
-    plt.show()
+    ############################################
+
+    plt.figure(4)
+    plt.grid()
+    plt.title("Domain: Rate Of Convergence vs Iters")
+    plt.plot(DomainRatios[:cut])
+    plt.xlabel("iters")
+    plt.legend(["Dom. Rate Of Conv."])
+    plt.savefig("dom-roc.pdf", format="pdf", bbox_inches="tight")
+
+    ############################################
+    
+    plt.figure(5)
+    plt.grid()
+    plt.title("Co-Domain: Rate Of Convergence vs Iters")
+    plt.plot(ImageRatios[:cut])
+    plt.xlabel("iters")
+    plt.legend(["Co-Dom. Rate Of Conv."])
+    plt.savefig("co-dom-roc.pdf", format="pdf", bbox_inches="tight")
+
+    ############################################
+    
+    plt.figure(6)
+    plt.grid()
+    plt.title("Metrics vs Iters")
+    plt.semilogy(DomainDistance[:cut])
+    plt.semilogy(ImgDistance[:cut])
+    plt.xlabel("iters")
+    plt.legend(["Domain Distance", "Image Distance"])
+    plt.savefig("metr-vs-iters.pdf", format="pdf", bbox_inches="tight")
+    
+    ############################################
+
+    plt.figure(7)
+    plt.grid()
+    plt.title("Metrics vs Cpu Time")
+    plt.semilogy(CpuTimes[:cut], DomainDistance[:cut])
+    plt.semilogy(CpuTimes[:cut], ImgDistance[:cut])
+    plt.xlabel("Runtime (s)")
+    plt.legend(["Domain Distance", "Image Distance"])
+    plt.savefig("metr-vs-cputime.pdf", format="pdf", bbox_inches="tight")
+
+
+    #plt.show()
 
 
 Plot(sys.argv[1])
